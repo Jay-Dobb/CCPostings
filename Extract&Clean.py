797e1c45d1b73f7bf2bcd15c6ac99e71ef8544fc
Jay-Dobb <107642835+Jay-Dobb@users.noreply.github.com>
2022-06-19 06:50:41 -0500

    Add files via upload

--- Extract&Clean.py
+++ Extract&Clean.py
@@ -12,17 +12,26 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
 
 PATH = "C:\Tools\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
-driver.get( 'https://yakima.craigslist.org/search/moxee-wa/jjj?query=%28remote+%7C+%22work+from+home%22%29+-%28%22%2421.25%22%29+-%28lawyer%29+-%28%22law+program%22%29+-%28cleaner%29+-%28hairdresser%29+-%28%22Free+Employment%22%29+-driver+-mother&lat=46.7221&lon=-120.1035&sort=date&search_distance=250')
 
+#driver.get( 'https://yakima.craigslist.org/search/moxee-wa/jjj?query=%28remote+%7C+%22work+from+home%22%29+-%28%22%2421.25%22%29+-%28lawyer%29+-%28%22law+program%22%29+-%28cleaner%29+-%28hairdresser%29+-%28%22Free+Employment%22%29+-driver+-mother&lat=46.7221&lon=-120.1035&sort=date&search_distance=250')
+
+gc = gspread.service_account(filename='mycredentials.json')
+
+#Craigslist URLS
+craiglistURLS = []
+urlNum = 0
 
 #Scraping All Listings Information from site for current day
 allTitleListings = []
 allHyperlinksListings = []
 allDatesListing = []
-allLocationListing = []
+# allLocationListing = [] #not yet used
 allCityListing = []
 fullBodyText = []
 
@@ -30,33 +39,37 @@ fullBodyText = []
 noDupTitleListings = []
 noDupHyperlinksListings = []
 noDupDatesListing = []
-noDupLocationListing = []
+#noDupLocationListing = [] #not yet used
 noDupCityListing = []
 slimBodyText =[]
 
 combinedInfoListings = []
 
 
-def datetext():
+def currentDate():
     today = date.today()
     print("Today's date:", today)
-    d4 = today.strftime("%b-%d")
-    #print("d4 =", d4)
+
+    if stopAtdate == 'Today':
+        d4 = today.strftime("%b-%d")
+        #print("d4 =", d4)
 
 
-    # converting number in date into Int from strong
-    dayint = int(d4[4:6])
-    # print("day number ", dayint)
-    # print("day number type", type(dayint))
+        # converting number in date into Int from strong
+        dayint = int(d4[4:6])
+        # print("day number ", dayint)
+        # print("day number type", type(dayint))
 
-    if dayint > 9:
-        d5 = d4[:3] + " " + d4[4:6]
-        #print("d5 = ", d5)
+        if dayint > 9:
+            d5 = d4[:3] + " " + d4[4:6]
+            #print("d5 = ", d5)
+        else:
+            d5 = d4[:3] + " " + d4[5:6]
+            #print("d5 = ", d5)
+        return d5
+
     else:
-        d5 = d4[:3] + " " + d4[5:6]
-        #print("d5 = ", d5)
-
-    return d5
+        print("Scraping List from: ", stopAtdate)
 
 def craigslistCatergories():  #maybe add this to a seperate code page, it calls this not to overload the memory
 
@@ -106,10 +119,31 @@ def craigslistCatergories():  #maybe add this to a seperate code page, it calls 
         #fullTime = 0
         #pass
 
-def LocationListings():
+def injectCraigslistURLS():
+
+    #WestCoast North WASH/Van
+    craiglistURLS.append('https://yakima.craigslist.org/search/moxee-wa/jjj?sort=date&lat=46.7221&lon=-120.1035&search_distance=250')
+
+    #WestCoast South Cali/Nevada
+    #craiglistURLS.append('https://visalia.craigslist.org/search/independence-ca/jjj?sort=date&lat=36.7973&lon=-117.6027&search_distance=250')
+
+def locationListings():
     #something something
     # scrap from Timezone/County/Radius Cordninates on all of West Coast | then Mid Cost
-    print("test")
+    pass
+
+def filterSearch(urlNum):
+
+    print("filtersearch url", urlNum)
+
+    driver.get(craiglistURLS[urlNum])
+    searchbox = driver.find_element(By.CSS_SELECTOR, '.querybox')
+    # searchField = searchbox.find_element(By.TAG_NAME, 'input').get_attribute('value')
+    # print("what is currently in searchbox", searchField)
+
+    inputbox = searchbox.find_element(By.NAME, 'query')
+    inputbox.send_keys(craigslistSearchFilter)
+    inputbox.send_keys(Keys.RETURN)
 
 def webscrapTitlesList():
 
@@ -133,6 +167,9 @@ def webscrapTitlesList():
             #HYPERLINK
             # within <a/> find attritube href (hyperlink)
             hyperLinkitem = webEle.find_element(By.TAG_NAME, 'a').get_attribute('href')
+            #hyperLinkitem = hyperLinkitem[8:]
+
+            #print("sliced out https: ",hyperLinkitem)
             #print(hyperitem)
             #print(eachitem + 1)
 
@@ -157,9 +194,10 @@ def webscrapTitlesList():
                     break
 
     driver.quit()
-    print(stopAtdate,": All Posting Items = ",eachitem)
+    print(stopAtdate, ": All Posting Items = ", eachitem)
 
 def titlesListclean():
+    # Removes Duplicate names in main list and aligns list index for each other list and deletes those itmes
     nameSet = set()
     item = 0
     for tt, hyper, dd, city in zip(allTitleListings,allHyperlinksListings,allDatesListing,allCityListing):
@@ -181,6 +219,7 @@ def titlesListclean():
     combinedInfoListings.pop(len(combinedInfoListings) - 1)
     print(stopAtdate,": Postings Items After Cleaned Duplicates = ",len(combinedInfoListings))
     print("----- Scraping & Formatting.. ----- ")
+    print(" ")
 
 def webscrapBodyText():
 
@@ -232,21 +271,23 @@ def bodytextListSlim():
                 bodytext = bodytext[0:len(bodytext)-400] + bodytext[len(bodytext)-200:]
                 #print("new length of body each time", len(bodytext))
             else:
-                print("new length of body letters: ", len(bodytext)) #finished while loop
+                #print("new length of body letters: ", len(bodytext)) #finished while loop
+                pass
 
         else:
-            print("Length of body text less than ",amount)
+            #print("Length of body text less than ",amount)
             #bodytextListSlim().append(bodytext)
+            pass
 
         #Ended while conditonal, so add slimmed body text
         slimBodyText.append(bodytext)
 
-        print("HyperLink index:",index," ",noDupHyperlinksListings[index])
+        #print("HyperLink index:",index," ",noDupHyperlinksListings[index])
 
         #slice bodytext to preview output with skim
         skimBody = (slimBodyText[index])
-        print("Bodytext Index:",index," ",skimBody[0:22])
-        print("       ")
+        #print("Bodytext Index:",index," ",skimBody[0:22])
+        #print("       ")
 
         index = index + 1
 
@@ -254,13 +295,48 @@ def bodytextATSfilterkeywords():
     #further filter with python manipulation
     print("test")
 
+def injectToSheets():
+
+    scope=   ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+    credentials=ServiceAccountCredentials.from_json_keyfile_name('mycredentials.json',scope)
+    gc=gspread.authorize(credentials)
+    wks=gc.open('Sele').sheet1
+
+    cListing = 0
+
+    #Slices all strings in hyperlink list to remove https://
+    stripHyperlinksListing = [w[8:] for w in noDupHyperlinksListings]
+
+    for items in noDupTitleListings:
+
+        wks.append_row([noDupDatesListing[cListing], stripHyperlinksListing[cListing], noDupTitleListings[cListing], noDupCityListing[cListing]])
+        #[gColDate,gColLocation,gColTitle,gColHyperComment,created_on,updated_on] google sheet field    #names.
+        print("Index ", cListing, " Inserted Successfully ! Title:", noDupTitleListings[cListing])
+        cListing = cListing + 1
+
+def injectBodyCommentsToSheet():
+
+    driver.get('https://docs.google.com/spreadsheets/d/14Ns_4P9OVNf_Z2pzQ2KVrdzzmjmip9etf2-zZHJmdOw/edit?usp=sharing')
+
+    combine_keys = ActionChains(driver)
+    time.sleep(1)
+    actions = ActionChains(driver)
+    actions.send_keys(Keys.ARROW_DOWN).perform()
+    actions.send_keys(Keys.ARROW_RIGHT).perform()
+    actions.send_keys(Keys.ARROW_RIGHT).perform()
+    time.sleep(1)
+    combine_keys.key_down(Keys.ALT).key_down(Keys.CONTROL).send_keys("m").key_up(Keys.ALT).key_up(Keys.CONTROL).perform()
+    actions.send_keys('craigslist job description')
+    actions.send_keys(Keys.ENTER).perform()
+    actions.send_keys(Keys.ENTER).perform()
+
 def resultsToNotepad():
     print("- - - - - - - - -")
     print("Packing Data Into Notepad...")
 
     index = 0
 
-    with open('readme.txt', 'w') as f:
+    with open('ListOutput.txt', 'w') as f:
         for x in noDupTitleListings:
             f.write(str(combinedInfoListings[index] + slimBodyText[index]))
             f.write('\n')
@@ -283,6 +359,12 @@ def resultsToNotepad():
         pass
 
 #----------------------------------------------
+#TO DO NEXT
+# Manipulate GoogleSheets
+# scrap by categories grouped
+# scrap from Timezone/County/Radius Cordninates on all of West Coast | then Mid Cost
+
+printProcessDebug = True  #If True will print all outputs in each function to see whats going on (Not seutp yet)
 
 # Modifiers
 # Filter craigslist wildcards
@@ -290,30 +372,56 @@ def resultsToNotepad():
 # Date To Stop scrap(Current Date)
 # Reduce Body text posting amount
 
-#'https://yakima.craigslist.org/search/moxee-wa/jjj?query=%28remote+%7C+%22work+from+home%22%29+-%28%22%2421.25%22%29+-%28lawyer%29+-%28%22law+program%22%29+-%28cleaner%29+-%28hairdresser%29+-%28%22Free+Employment%22%29+-driver+-mother&lat=46.7221&lon=-120.1035&sort=date&search_distance=250"
-#TO DO NEXT
-
-
-# Manipulate GoogleSheets
-# scrap by categories grouped
-# scrap from Timezone/County/Radius Cordninates on all of West Coast | then Mid Cost
-
-
-
-
-craigslistSearchFilter = "... "
-stopAtdate = datetext()
-stopscrapAtList = 6
+craigslistSearchFilter = '(remote | "work from home") -("$21.25") -(lawyer) -("law program") -(cleaner) -(hairdresser) -("Free Employment") -driver -mother'
+stopAtdate = "Jun 18"
+#stopAtdate = input("Type Today or the Date to Scrape: ")   #Specify Date or Put in 'Today'
+stopscrapAtList = 3
 shortenBodyTextLettersTo = 1900
 
+def sheettest():
+
+    scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('mycredentials.json',scope)
+    gc = gspread.authorize(credentials)
+    wks = gc.open('Sele').sheet1
 
 
-webscrapTitlesList()
+    cella1 = wks.get_values("A1")
+    print(cella1)
 
-titlesListclean()
 
-webscrapBodyText()
+    #range = wks.getRange('B5')
+    #range.activate()
 
-bodytextListSlim()
+    #selection = sheet.getSelection()
+    #currentCell = B5
+    #currentCell = selection.getCurrentCell()
 
-resultsToNotepad()
+injectBodyCommentsToSheet()
+
+'''
+
+#make this in new py file so it doesnt over load memory?
+injectCraigslistURLS()
+for x in craiglistURLS:
+    print("In Url list Index", urlNum)
+    #
+    currentDate()
+    #
+    filterSearch(urlNum)
+    #
+    webscrapTitlesList()
+    # nin
+    titlesListclean()
+    # ojo
+    webscrapBodyText()
+    # ihih
+    bodytextListSlim()
+    # hihi
+    injectToSheets()
+    #
+    #resultsToNotepad()
+    urlNum = urlNum + 1
+    time.sleep(5)
+    
+'''
